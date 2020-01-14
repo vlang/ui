80c130f210b98e2ed5ae4ee62385fbd71dc1574f
Delyan Angelov <delian66@gmail.com>
2020-01-14 22:53:18 +0200

    Pass cli args to gtk_init too.

--- ui_linux.v
+++ ui_linux.v
@@ -3,6 +3,7 @@
 // that can be found in the LICENSE file.
 module ui
 
+import os
 #include <gtk/gtk.h>
 #flag `pkg-config --cflags gtk+-3.0` `pkg-config --libs gtk+-3.0`
 
@@ -10,7 +11,7 @@ type Callback fn (voidptr)
 struct C.GtkWidget{}
 struct C.GtkDialogFlags{}
 struct C.GtkDialog{}
-fn C.gtk_init(int, voidptr)
+fn C.gtk_init(int, &charptr)
 fn C.gtk_main_quit()
 fn C.gtk_main()
 fn C.gtk_message_dialog_new(voidptr, int, int, int, charptr) &GtkWidget
@@ -31,7 +32,7 @@ fn message_box_close(w &GtkWidget){
 }
 
 pub fn message_box(s string) {
-	gtk_init(0, [''].data)
+	gtk_init(os.args.len - 1, os.args.data)
 	dialog := C.gtk_message_dialog_new( 0, C.GTK_DIALOG_DESTROY_WITH_PARENT, C.GTK_MESSAGE_INFO, C.GTK_BUTTONS_OK, s.str)
 	C.g_signal_connect_swapped( dialog, response_signal_name.str, message_box_close as Callback, dialog )
 	C.gtk_widget_show(dialog)
