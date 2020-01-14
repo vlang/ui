3128590df7a36b6cdd26d0524f83b118907b98bc
Charles WANG <changrui@users.noreply.github.com>
2020-01-14 16:46:02 +0800

    Create const.v

---
+++ const.v
@@ -0,0 +1,20 @@
+// v ui public consts
+module ui
+
+public const (
+  font_dir = []string{
+    r'c:\windows\fonts\arial.ttf',
+    
+  }
+  
+  // error massages
+  img_notfound = 'Image file not found.'
+  font_notfound = 'System font file not found.'
+  glfw_notinstalled = 'glfw not installed.'
+  freetype_notinstalled = 'freetype is not installed.'
+  
+  // default size
+  default_font_size = 13
+  default_height = 20
+  
+)
