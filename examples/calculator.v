41bc61bba548c438832d669783ddda920a231827
Delyan Angelov <delian66@gmail.com>
2020-01-18 21:45:02 +0200

    Fix operations too (no priorities for now)

--- examples/calculator.v
+++ examples/calculator.v
@@ -1,10 +1,12 @@
 import ui
 
+import math
+
 const (
 	buttons_per_row = 4
 	bwidth = 30
 	bheight = 30
-	bpadding = 5
+	bpadding = 5	
 )
 
 
@@ -13,18 +15,25 @@ mut:
 	txtbox     &ui.TextBox
 	window     &ui.Window
 	btns       []&ui.Button
-	op         string
 	result     f64
 	is_float   bool
 	new_number bool
+	operands   []f64
+	operations []string
 }
 
 fn main() {
-	ops := ['C', '', '', '÷', '7', '8', '9', '*', '4', '5', '6', '-', '1', '2', '3', '+', '0', '.', '', '=']
+	ops := [
+		'C', ' ', ' ', '÷',
+		'7', '8', '9', '*',
+		'4', '5', '6', '-',
+		'1', '2', '3', '+',
+		'0', '.', '±', '='
+	]
 	mut app := &App{txtbox:0, window:0}
 	app.window = ui.new_window({
-		width: 155
-		height: 220
+		width: 145
+		height: 210
 		title: 'V Calc'
 		user_ptr: app
 	})
@@ -32,8 +41,8 @@ fn main() {
 		placeholder: '0'
 		parent: app.window
 		width: 135
-		x: 10
-		y: 10
+		x: 5
+		y: 5
 		read_only: true
 	})
 	for i, op in ops {
@@ -45,70 +54,132 @@ fn main() {
 fn btn_click(app mut App, btn &ui.Button) {
 	op := btn.text
 	number := app.txtbox.text
+	
 	if op == 'C' {
 		app.result = 0
-		app.op = ''
+		app.operands = []
+		app.operations = []
 		app.new_number = true
-		app.txtbox.set_text('0')
 		app.is_float = false
+		app.update_result()
 		return
 	}
+	
 	if op[0].is_digit() || op == '.' {
 		// Can only have one `.` in a number
 		if op == '.'  && number.contains('.') {
 			return
 		}
-		if app.txtbox.text.len >= 17 {
-			return
-		}
-		// First click, replace the zero
-		if number == '0' {
+		if app.new_number {
 			app.txtbox.set_text(btn.text)
-		}
-		else {
-			if app.new_number {
-				app.txtbox.set_text(btn.text)
-				app.new_number = false
-			}	else {
-				// Append a new digit
-				app.txtbox.set_text(number + btn.text)
-			}
+			app.new_number = false
+			app.is_float = false
+		}	else {
+			// Append a new digit
+			app.txtbox.set_text(number + btn.text)
 		}
 		return
 	}
-	// User pressed + etc several times, ignore it
-	if app.new_number {
-		return
-	}
+	
 	if number.contains('.') {
 		app.is_float = true
 	}
-	if op in ['+', '-'] {
-		app.op = op
-		if op == '+' {
-			app.result += number.int()
-		} else if op == '-' {
-			app.result -= number.int()
+	
+	if op in ['+', '-', '÷', '*', '±', '='] {
+		if !app.new_number {
+			app.new_number = true
+			app.operands << number.f64()
 		}
-		app.new_number  = true
+		app.operations << op
+		app.calculate()
 	}
-	else if op == '=' {
-	}
-	// Format and print the result
-	if app.is_float {
-		app.txtbox.set_text(app.result.str())
-	}
-	else {
+	app.update_result()
+}
+
+fn (app mut App) update_result() {
+	// Format and print the result	
+	if !math.trunc(app.result).eq( app.result ) {
+		app.txtbox.set_text('${app.result:-15.10f}')
+	}else{
 		app.txtbox.set_text(int(app.result).str())
 	}
 }
 
+fn pop_f64(a []f64) (f64, []f64) {
+	res := a.last()
+	return res, a[0..a.len-1]
+}
+fn pop_string(a []string) (string, []string) {
+	res := a.last()
+	return res, a[0..a.len-1]
+}
+
+fn (app mut App) calculate() {
+	mut a := f64(0)
+	mut b := f64(0)
+	mut op := ''	
+	mut operands := app.operands
+	mut operations := app.operations
+	mut result := if operands.len == 0 { f64(0.0) } else { operands.last() }
+	mut i := 0
+	for {
+		i++
+		if operations.len == 0 { break }
+		op, operations = pop_string( operations )
+		if op == '=' {
+			continue
+		}
+		if operands.len < 1 {
+			operations << op
+			break
+		}
+		b, operands = pop_f64( operands )		
+		if op == '±' {
+			result = - b
+			operands << result
+			continue
+		}
+		if operands.len < 1 {
+			operations << op
+			operands << b
+			break
+		}
+		a, operands = pop_f64( operands )
+		match op {
+			'+' { result = a + b }
+			'-' { result = a - b }
+			'*' { result = a * b }
+			'÷' {
+				if b.eq( f64(0.0) ) {
+					eprintln('Division by zero!')
+					b = 0.0000000001
+				}
+				result = a / b
+			} else {
+				operands << a
+				operands << b
+				result = b
+				eprintln('Unknown op: $op ')
+				break
+			}
+		}
+		operands << result
+		//eprintln('i: ${i:4d} | res: ${result} | op: $op | operands: $operands | operations: $operations')
+	}
+	app.operations = operations
+	app.operands = operands
+	app.result = result
+	//eprintln('----------------------------------------------------')
+	//eprintln('Operands: $app.operands  | Operations: $app.operations ')
+	//eprintln('-------- result: $result | i: $i -------------------')
+}
+
 fn (app mut App) add_button(text string, button_idx int) {
 	// Calculate button's coordinates from its index
-	x := 2*bpadding + (button_idx % buttons_per_row) * (bwidth+bpadding)
-	y := 2*bpadding + bheight + (button_idx / buttons_per_row) * (bwidth+bpadding)
+	x := bpadding + (button_idx % buttons_per_row) * (bwidth+bpadding)
+	y := bpadding + bheight + (button_idx / buttons_per_row) * (bwidth+bpadding)
 	// Skip empty buttons
-	if text != '' {
+	if text != ' ' {
 		app.btns << ui.new_button({
 			text: text
 			x: x
