a3cc0d56df22707666ef092ceb049ddfe7ceb2ec
Leah Lundqvist <hampus0505@gmail.com>
2020-01-24 18:24:48 +0100

    add multi line support to label

--- label.v
+++ label.v
@@ -41,13 +41,20 @@ fn (b mut Label) set_pos(x, y int) {
 }
 
 fn (b mut Label) propose_size(w, h int) (int, int) {
-	//b.width = w
-	//b.height = h
-	return b.ui.ft.text_size(b.text)
+	size := b.ui.ft.text_size(b.text)
+
+	// First return the width, then the height multiplied by line count.
+	return size.var_0, size.var_1 * b.text.split('\n').len
 }
 
 fn (b mut Label) draw() {
-	b.ui.ft.draw_text(b.x, b.y, b.text, btn_text_cfg)
+	splits := b.text.split('\n') // Split the text into an array of lines.
+	height := b.ui.ft.text_height('W') // Get the height of the current font.
+
+	for i, split in splits {
+		// Draw the text at b.x and b.y + line height * current line
+		b.ui.ft.draw_text(b.x, b.y + (height * i), split, btn_text_cfg)
+	}
 }
 
 fn (t &Label) focus() {}
