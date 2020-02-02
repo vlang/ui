5afdaca05f89e47ae453d45651f4d8d52cd6680d
Delyan Angelov <delian66@gmail.com>
2020-02-02 22:53:14 +0200

    Fix warnings when compiling examples/rectangles.v .

--- examples/rectangles.v
+++ examples/rectangles.v
@@ -12,7 +12,7 @@ mut:
 }
 
 fn main() {
-	mut app := &App{}
+	mut app := &App{window:0}
 	window := ui.window({
 		width: win_width
 		height: win_height
@@ -24,10 +24,10 @@ fn main() {
 			spacing: 5
 			margin: ui.MarginConfig{5,5,5,5}
 		}, [
-			ui.rectangle({ height: 64, width: 64, color: gx.Color { r: 255, g: 100, b: 100 } }) as ui.IWidgeter,
-			ui.rectangle({ height: 64, width: 64, color: gx.Color { r: 255, g: 100, b: 100 }, border: true, border_color: gx.Color { r: 0, g: 0, b: 0 }}),
-			ui.rectangle({ height: 64, width: 64, color: gx.Color { r: 255, g: 100, b: 100 }, radius: 24 }),
-			ui.rectangle({ height: 64, width: 64, color: gx.Color { r: 255, g: 100, b: 100 }, radius: 24, border: true, border_color: gx.Color { r: 0, g: 0, b: 0 }})
+			ui.rectangle({ ref: 0 height: 64, width: 64, color: gx.Color { r: 255, g: 100, b: 100 } }) as ui.IWidgeter,
+			ui.rectangle({ ref: 0 height: 64, width: 64, color: gx.Color { r: 255, g: 100, b: 100 }, border: true, border_color: gx.Color { r: 0, g: 0, b: 0 }}),
+			ui.rectangle({ ref: 0 height: 64, width: 64, color: gx.Color { r: 255, g: 100, b: 100 }, radius: 24 }),
+			ui.rectangle({ ref: 0 height: 64, width: 64, color: gx.Color { r: 255, g: 100, b: 100 }, radius: 24, border: true, border_color: gx.Color { r: 0, g: 0, b: 0 }})
 		]) as ui.IWidgeter
 	])
 
