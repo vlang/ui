72f595645ca41ff27ea175d5eb15c16288e5b774
origel <zhurx4g@qq.com>
2020-02-07 08:00:08 +0800

    change get_size

--- ui.v
+++ ui.v
@@ -72,7 +72,7 @@ pub interface IWidgeter {
 pub interface ILayouter {
 	get_ui() &UI
 	get_user_ptr() voidptr
-	get_size() (int, int)
+	size() (int, int)
 	get_subscriber() &eventbus.Subscriber
 	//on_click(ClickFn)
 	unfocus_all()
