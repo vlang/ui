320df07219ff71c453b4b4d7baad86b440fb3b83
progamateur <58486307+progamateur@users.noreply.github.com>
2020-04-08 16:25:03 +0200

    Update slider.v
    . Format source: v fmt  -w  lider.v

--- examples/slider.v
+++ examples/slider.v
@@ -19,33 +19,30 @@ fn main() {
 		height: win_height
 		title: 'Slider Example'
 		user_ptr: app
-	}, [
-		ui.iwidget(ui.row({
-			stretch: true
-			alignment: .center
-			margin: ui.MarginConfig{5,5,5,5}
-			spacing: 10
-		}, [
-			ui.iwidget(ui.slider({
-				width: 20
-				height: 200
-				orientation: .vertical
-				max: 100
-				val: 0
-				on_value_changed: on_vert_value_changed
-				ref: &app.vert_slider
-			})),
-			ui.slider({
-				width: 200
-				height: 20
-				orientation: .horizontal
-				max: 100
-				val: 0
-				on_value_changed: on_hor_value_changed
-				ref: &app.hor_slider
-			})
-		]))
-	])
+	}, [ui.iwidget(ui.row({
+		stretch: true
+		alignment: .center
+		margin: ui.MarginConfig{
+			5,5,5,5}
+		spacing: 10
+	}, [ui.iwidget(ui.slider({
+		width: 20
+		height: 200
+		orientation: .vertical
+		max: 100
+		val: 0
+		on_value_changed: on_vert_value_changed
+		ref: &app.vert_slider
+	})),
+	ui.slider({
+		width: 200
+		height: 20
+		orientation: .horizontal
+		max: 100
+		val: 0
+		on_value_changed: on_hor_value_changed
+		ref: &app.hor_slider
+	})]))])
 	app.window = window
 	ui.run(window)
 }
