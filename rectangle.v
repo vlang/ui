a275e6e30780547d255ae7ae3d3d98610fc8db06
progamateur <58486307+progamateur@users.noreply.github.com>
2020-04-15 00:17:18 +0200

    text field is displayed in the rectangle
    import block                  -> add freetype
    
    RectangleConfig struct  -> add text field
    
    rectangle function         -> text field is updated
    
    draw function                -> text is displayed inside the rectangle

--- rectangle.v
+++ rectangle.v
@@ -4,39 +4,46 @@
 module ui
 
 import gx
+import freetype
 
 pub struct Rectangle {
 mut:
-	text   string
-	parent ILayouter
-	x      int
-	y      int
-	height int
-	width  int
-	radius int
-	border bool
+	text         string
+	parent       ILayouter
+	x            int
+	y            int
+	height       int
+	width        int
+	radius       int
+	border       bool
 	border_color gx.Color
-	color  gx.Color
-	ui     &UI
+	color        gx.Color
+	ui           &UI
 }
 
 pub struct RectangleConfig {
-	height int
-	width  int
-	color  gx.Color
-	radius int = 0
-	border bool = false
-	border_color gx.Color = gx.Color { r: 180, g: 180, b: 190 }
-	ref		&Rectangle
+	text         string
+	height       int
+	width        int
+	color        gx.Color
+	radius       int=0
+	border       bool=false
+	border_color gx.Color=gx.Color {
+		r: 180
+		g: 180
+		b: 190
+	}
+	ref          &Rectangle
 }
 
-fn (r mut Rectangle)init(parent ILayouter) {
+fn (r mut Rectangle) init(parent ILayouter) {
 	ui := parent.get_ui()
 	r.ui = ui
 }
 
 pub fn rectangle(c RectangleConfig) &Rectangle {
 	rect := &Rectangle{
+		text: c.text
 		height: c.height
 		width: c.width
 		radius: c.radius
@@ -57,28 +64,56 @@ fn (r mut Rectangle) set_pos(x, y int) {
 	r.y = y
 }
 
-fn (b mut Rectangle) size() (int, int) {
-	return b.width, b.height
+fn (b mut Rectangle) size() (int,int) {
+	return b.width,b.height
 }
 
-fn (r mut Rectangle) propose_size(w, h int) (int, int) {
-	return r.width, r.height
+fn (r mut Rectangle) propose_size(w, h int) (int,int) {
+	return r.width,r.height
 }
 
 fn (r mut Rectangle) draw() {
 	if r.radius > 0 {
 		r.ui.gg.draw_rounded_rect(r.x, r.y, r.width, r.height, r.radius, r.color)
-		if r.border { r.ui.gg.draw_empty_rounded_rect(r.x, r.y, r.width, r.height, r.radius, r.border_color) }
-	} else {
+		if r.border {
+			r.ui.gg.draw_empty_rounded_rect(r.x, r.y, r.width, r.height, r.radius, r.border_color)
+		}
+	}
+	else {
 		r.ui.gg.draw_rect(r.x, r.y, r.width, r.height, r.color)
-		if r.border { r.ui.gg.draw_empty_rect(r.x, r.y, r.width, r.height, r.border_color) }
+		if r.border {
+			r.ui.gg.draw_empty_rect(r.x, r.y, r.width, r.height, r.border_color)
+		}
+	}
+	text_cfg := gx.TextCfg{
+		color: gx.red
+		size: freetype.default_font_size
+		align: gx.ALIGN_LEFT
+		max_width: r.width
+	}
+	// Display rectangle text
+	if r.text != '' {
+		text_width,text_height := r.ui.ft.text_size(r.text)
+		mut dx := (r.width - text_width) / 2
+		mut dy := (r.height - text_height) / 2
+		if dx < 0 {
+			dx = 0
+		}
+		if dy < 0 {
+			dy = 0
+		}
+		r.ui.ft.draw_text(r.x + dx, r.y + dy, r.text, text_cfg)
 	}
 }
 
 fn (r &Rectangle) focus() {}
 
-fn (r &Rectangle) is_focused() bool { return false }
+fn (r &Rectangle) is_focused() bool {
+	return false
+}
 
 fn (r &Rectangle) unfocus() {}
 
-fn (r &Rectangle) point_inside(x, y f64) bool { return false }
+fn (r &Rectangle) point_inside(x, y f64) bool {
+	return false
+}
