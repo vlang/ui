69c9be146f014de22792a1d65e308a126458e934
progamateur <58486307+progamateur@users.noreply.github.com>
2020-04-29 19:35:14 +0200

    rename ui_windows.c.v and ui.linux.c.v (V 0.1.25 comp.)

---
+++ ui_windows.v
@@ -0,0 +1,12 @@
+// Copyright (c) 2020 Alexander Medvednikov. All rights reserved.
+// Use of this source code is governed by a GPL license
+// that can be found in the LICENSE file.
+module ui
+
+fn C.MessageBox(h voidptr, text charptr, caption charptr, kind u32) int
+
+pub fn message_box(s string) {
+	title := ''
+	C.MessageBox(0, s.to_wide(), title.to_wide(), C.MB_OK)
+}
+
