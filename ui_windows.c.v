a4fec6a676b0541a7fbfac103d1736c3ddfad9c6
progamateur <58486307+progamateur@users.noreply.github.com>
2020-05-19 02:11:39 +0200

    Add set_process_dpi_awareness function

--- ui_windows.c.v
+++ ui_windows.c.v
@@ -2,6 +2,21 @@
 // Use of this source code is governed by a GPL license
 // that can be found in the LICENSE file.
 module ui
+#flag -L @VROOT/shcore/x64
+#flag -l shcore
+
+#define ui__setprocessdpiwareness SetProcessDpiAwareness
+
+/* PROCESS_DPI_AWARENESS constants */
+const (
+	PDpiA = '> Process Dpi Awareness:'
+	Process_DPI_Unaware = 0
+	Process_System_DPI_Aware = 1
+	Process_Per_Monitor_DPI_Aware = 2
+)
+
+/* pub is needed for msvc compiler compatibility */
+pub fn setprocessdpiwareness(process_dpi_awareness int) voidptr
 
 fn C.MessageBox(h voidptr, text charptr, caption charptr, kind u32) int
 
@@ -10,3 +25,18 @@ pub fn message_box(s string) {
 	C.MessageBox(0, s.to_wide(), title.to_wide(), C.MB_OK)
 }
 
+
+/*
+	SetProcessDpiAwareness function of Microsoft shcore.dll tested withcompilers:
+	- gcc.exe (x86_64-posix-seh-rev0, Built by MinGW-W64 project) 8.1.0
+	- Microsoft MSVC cl.exe C/C++ version 19.25.28614 for x64
+*/
+pub fn set_process_dpi_awareness(awareness int) {
+	match awareness {
+		0 { println( PDpiA + if setprocessdpiwareness( Process_DPI_Unaware) == 0  { ' Process_DPI_Unaware' } else { ' 0 change failed' }) }
+		1 { println( PDpiA + if setprocessdpiwareness( Process_System_DPI_Aware) == 0  { ' Process_System_DPI_Aware' } else { ' 1 change failed'}) }
+		2 { println( PDpiA + if setprocessdpiwareness( Process_Per_Monitor_DPI_Aware) == 0  { ' Process_Per_Monitor_DPI_Aware' } else { ' 2 change failed'}) }
+		else { println( PDpiA  + ' no change') }
+		}
+	println('')
+}
