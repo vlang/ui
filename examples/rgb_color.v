839d2c0fc9da0515c389a7a3ca4d81a8a0845b2e
progamateur <58486307+progamateur@users.noreply.github.com>
2020-05-24 00:02:47 +0200

    Fix V UI 0.0.3 compatibilities
    - No more widgets config
    - Deletion of unnecessary commented lines

--- examples/rgb_color.v
+++ examples/rgb_color.v
@@ -28,125 +28,114 @@ mut:
 }
 
 fn main() {
-	// widget config
-	rgb_rectangle_cfg := ui.RectangleConfig{
-		width: 200
-		height: 100
-		border: true
-		color: gx.Color{
-			r: slider_val
-			g: slider_val
-			b: slider_val
-		}
-	}
-	r_textbox_cfg := ui.TextBoxConfig{
-		width: 40
-		height: 20
-		max_len: 3
-		read_only: false
-		is_numeric: true
-		on_key_up: on_r_key_up
-		text: -1
-	}
-	g_textbox_cfg := ui.TextBoxConfig{
-		width: 40
-		height: 20
-		max_len: 3
-		read_only: false
-		is_numeric: true
-		on_key_up: on_g_key_up
-		text: -1
-	}
-	b_textbox_cfg := ui.TextBoxConfig{
-		width: 40
-		height: 20
-		max_len: 3
-		read_only: false
-		is_numeric: true
-		on_key_up: on_b_key_up
-		text: -1
-	}
-	r_slider_cfg := ui.SliderConfig{
-		width: 16
-		height: 200
-		orientation: .vertical
-		min: slider_min
-		max: slider_max
-		val: slider_val
-		focus_on_thumb_only: true
-		rev_min_max_pos: true
-		on_value_changed: on_r_value_changed
-	}
-	g_slider_cfg := ui.SliderConfig{
-		width: 16
-		height: 200
-		orientation: .vertical
-		min: slider_min
-		max: slider_max
-		val: slider_val
-		focus_on_thumb_only: true
-		rev_min_max_pos: true
-		on_value_changed: on_g_value_changed
-	}
-	b_slider_cfg := ui.SliderConfig{
-		width: 16
-		height: 200
-		orientation: .vertical
-		min: slider_min
-		max: slider_max
-		val: slider_val
-		focus_on_thumb_only: true
-		rev_min_max_pos: true
-		on_value_changed: on_b_value_changed
-	}
-	// row config
-	textbox_row_cfg := ui.RowConfig{
-		alignment: .top
-		margin: ui.MarginConfig{120, 30, 30, 30}
-		spacing: 23
-	}
-	slider_row_cfg := ui.RowConfig{
-		alignment: .top
-		margin: ui.MarginConfig{ 150, 40, 30, 30 }
-		spacing: 38
-	}
-	rectangle_row_cfg := ui.RowConfig{
-		alignment: .top
-		margin: ui.MarginConfig{ 10, 10, 30, 30 }
-	}
-	label_row_cfg := ui.RowConfig{
-		alignment: .top
-		margin: ui.MarginConfig{ 354, 43, 30, 30 }
-		spacing: 54
-	}
-	
-	// app widgets
 	mut app := &App{
-		rgb_rectangle: ui.rectangle(rgb_rectangle_cfg)
-		r_textbox: ui.textbox(r_textbox_cfg)
-		g_textbox: ui.textbox(g_textbox_cfg)
-		b_textbox: ui.textbox(b_textbox_cfg)
-		r_slider: ui.slider(r_slider_cfg)
-		g_slider: ui.slider(g_slider_cfg)
-		b_slider: ui.slider(b_slider_cfg)
+		rgb_rectangle: ui.rectangle({
+			width: 200
+			height: 100
+			border: true
+			color: gx.Color{
+				r: slider_val
+				g: slider_val
+				b: slider_val
+			}
+		})
+		r_textbox: ui.textbox({
+			width: 40
+			height: 20
+			max_len: 3
+			read_only: false
+			is_numeric: true
+			on_key_up: on_r_key_up
+			text: -1
+		})
+		g_textbox: ui.textbox({
+			width: 40
+			height: 20
+			max_len: 3
+			read_only: false
+			is_numeric: true
+			on_key_up: on_g_key_up
+			text: -1
+		})
+		b_textbox: ui.textbox({
+			width: 40
+			height: 20
+			max_len: 3
+			read_only: false
+			is_numeric: true
+			on_key_up: on_b_key_up
+			text: -1
+		})
+		r_slider: ui.slider({
+			width: 16
+			height: 200
+			orientation: .vertical
+			min: slider_min
+			max: slider_max
+			val: slider_val
+			focus_on_thumb_only: true
+			rev_min_max_pos: true
+			on_value_changed: on_r_value_changed
+		})
+		g_slider: ui.slider({
+			width: 16
+			height: 200
+			orientation: .vertical
+			min: slider_min
+			max: slider_max
+			val: slider_val
+			focus_on_thumb_only: true
+			rev_min_max_pos: true
+			on_value_changed: on_g_value_changed
+		})
+		b_slider: ui.slider({
+			width: 16
+			height: 200
+			orientation: .vertical
+			min: slider_min
+			max: slider_max
+			val: slider_val
+			focus_on_thumb_only: true
+			rev_min_max_pos: true
+			on_value_changed: on_b_value_changed
+		})
 		r_label: ui.label({ text: 'R' })
-		g_label: ui.label({	text: 'G' })
+		g_label: ui.label({ text: 'G' })
 		b_label: ui.label({ text: 'B' })
 	}
 
 	app.r_textbox.text = &app.r_textbox_text
 	app.g_textbox.text = &app.g_textbox_text
 	app.b_textbox.text = &app.b_textbox_text
+
 	app.window = ui.window({
 		width: win_width
 		height: win_height
 		title: 'RGB color displayed in rectangle'
 		state: app
 	}, [
-		ui.row(textbox_row_cfg, [app.r_textbox, app.g_textbox, app.b_textbox]),
-		ui.row(slider_row_cfg, [app.r_slider, app.g_slider, app.b_slider]),
-		ui.row(rectangle_row_cfg, [app.rgb_rectangle]),
-		ui.row(label_row_cfg, [app.r_label, app.g_label, app.b_label])
+		ui.row({
+			alignment: .top
+			margin: ui.MarginConfig{120, 30, 30, 30}
+			spacing: 23
+		}, [app.r_textbox, app.g_textbox, app.b_textbox]),
+
+		ui.row({
+			alignment: .top
+			margin: ui.MarginConfig{ 150, 40, 30, 30 }
+			spacing: 38
+		}, [app.r_slider, app.g_slider, app.b_slider]),
+
+		ui.row({
+			alignment: .top
+			margin: ui.MarginConfig{ 10, 10, 30, 30 }
+		}, [app.rgb_rectangle]),
+
+		ui.row({
+			alignment: .top
+			margin: ui.MarginConfig{ 354, 43, 30, 30 }
+			spacing: 54
+		}, [app.r_label, app.g_label, app.b_label])
 	])
 	ui.run(app.window)
 }
@@ -172,8 +161,6 @@ fn on_b_value_changed(mut app App, slider &ui.Slider) {
 
 fn on_r_key_up(mut app App, textbox &ui.TextBox, keycode u32) {
 	if is_rgb_valid(app.r_textbox.text.int()) {
-		//text_adr :=  app.r_textbox.text
-		//text := *text_adr
 		app.r_slider.val = app.r_textbox_text.f32()
 		app.r_textbox.border_accentuated = false
 	} else {
@@ -184,8 +171,6 @@ fn on_r_key_up(mut app App, textbox &ui.TextBox, keycode u32) {
 
 fn on_g_key_up(mut app App, textbox &ui.TextBox, keycode u32) {
 	if is_rgb_valid(app.g_textbox.text.int()) {
-		//text_adr :=  app.g_textbox.text
-		//text := *text_adr
 		app.g_slider.val = app.g_textbox_text.f32()
 		app.g_textbox.border_accentuated = false
 	} else {
@@ -196,8 +181,6 @@ fn on_g_key_up(mut app App, textbox &ui.TextBox, keycode u32) {
 
 fn on_b_key_up(mut app App, textbox &ui.TextBox, keycode u32) {
 	if is_rgb_valid(app.b_textbox.text.int()) {
-		//text_adr :=  app.b_textbox.text
-		//text := *text_adr
 		app.b_slider.val = app.b_textbox_text.f32()
 		app.b_textbox.border_accentuated = false
 	} else {
