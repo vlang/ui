646f81537f2e3c28891a7605a32b2df6329a4ed5
Alexander Medvednikov <alexander@vlang.io>
2020-07-06 20:51:19 +0200

    remove glfw references on linux

--- ui_linux.c.v
+++ ui_linux.c.v
@@ -82,14 +82,14 @@ fn run_message_dialog(mut message_app MessageApp, s string){
 fn msgbox_on_key_down(mut app MessageApp, e &KeyEvent, window &Window ) {
 	match e.key {
 		.enter, .escape, .space {
-			app.window.glfw_obj.set_should_close(true)
+			//app.window.glfw_obj.set_should_close(true)
 		}
 		else {}
 	}
 }
 
 fn msgbox_btn_ok_click(mut app MessageApp) {
-	app.window.glfw_obj.set_should_close(true)
+	//app.window.glfw_obj.set_should_close(true)
 }
 
 fn word_wrap_to_lines(s string, max_line_length int) []string {
