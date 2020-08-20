e4e836ba2287491b0bacc8703eb240631592a2e2
Lukas Neubert <lukas.neubert3@gmail.com>
2020-08-20 13:41:52 +0200

    run fmt

--- examples/slider.v
+++ examples/slider.v
@@ -1,7 +1,7 @@
 import ui
 
 const (
-	win_width = 250
+	win_width  = 250
 	win_height = 250
 )
 
@@ -14,22 +14,22 @@ mut:
 
 fn main() {
 	mut app := &App{
-		hor_slider: ui.slider(
+		hor_slider: ui.slider({
 			width: 200
 			height: 20
 			orientation: .horizontal
 			max: 100
 			val: 0
 			on_value_changed: on_hor_value_changed
-		)
-		vert_slider: ui.slider(
+		})
+		vert_slider: ui.slider({
 			width: 20
 			height: 200
 			orientation: .vertical
 			max: 100
 			val: 0
 			on_value_changed: on_vert_value_changed
-		)
+		})
 		window: 0
 	}
 	app.window = ui.window({
@@ -41,12 +41,12 @@ fn main() {
 		ui.row({
 			stretch: true
 			alignment: .center
-			margin: ui.MarginConfig{5,5,5,5}
+			margin: ui.MarginConfig{5, 5, 5, 5}
 			spacing: 10
 		}, [
-			app.vert_slider
-			app.hor_slider
-		])
+			app.vert_slider,
+			app.hor_slider,
+		]),
 	])
 	ui.run(app.window)
 }
