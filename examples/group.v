fc858d0a685e4513459c4a9cf4fed0df02b9f68c
Lukas Neubert <lukas.neubert3@gmail.com>
2020-08-25 14:30:06 +0200

    vfmt changed files

--- examples/group.v
+++ examples/group.v
@@ -1,7 +1,7 @@
 import ui
 
 const (
-	win_width = 300
+	win_width  = 300
 	win_height = 300
 )
 
@@ -9,11 +9,13 @@ struct App {
 mut:
 	window     &ui.Window
 	first_name string = ''
-	last_name string = ''
+	last_name  string = ''
 }
 
 fn main() {
-	mut app := &App{window: 0}
+	mut app := &App{
+		window: 0
+	}
 	app.window = ui.window({
 		width: win_width
 		height: win_height
@@ -21,38 +23,38 @@ fn main() {
 		state: app
 	}, [
 		ui.group({
-		    x:20
-		    y:20
+			x: 20
+			y: 20
 			title: 'Group Demo'
 		}, [
-			ui.textbox(
+			ui.textbox({
 				max_len: 20
 				width: 200
 				placeholder: 'First name'
 				text: &app.first_name
-			)
-			ui.textbox(
+			}),
+			ui.textbox({
 				max_len: 50
 				width: 200
 				placeholder: 'Last name'
 				text: &app.last_name
-			)
-			ui.checkbox(
+			}),
+			ui.checkbox({
 				checked: true
 				text: 'Online registration1'
-			)
-			ui.checkbox(
+			}),
+			ui.checkbox({
 				checked: true
 				text: 'Online registration2'
-			)
-			ui.checkbox(
+			}),
+			ui.checkbox({
 				checked: true
 				text: 'Online registration3'
-			)
-			ui.button(
+			}),
+			ui.button({
 				text: 'Add user'
-			)
-		])
+			}),
+		]),
 	])
 	ui.run(app.window)
 }
