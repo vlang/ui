b848a7096703804cae738e529e502a48c4729b16
Lukas Neubert <lukas.neubert3@gmail.com>
2020-08-31 17:18:35 +0200

    stack: add license header

--- row.v
+++ row.v
@@ -1,4 +1,6 @@
-
+// Copyright (c) 2020 Alexander Medvednikov. All rights reserved.
+// Use of this source code is governed by a GPL license
+// that can be found in the LICENSE file.
 module ui
 
 pub struct RowConfig {
