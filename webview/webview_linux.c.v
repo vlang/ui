b630e6749f74b33ca10b3cb1dcba943979e25ffd
Lukas Neubert <serkonda@protonmail.com>
2020-12-08 17:34:41 +0100

    a gtk window can be opened

--- webview/webview_linux.c.v
+++ webview/webview_linux.c.v
@@ -8,13 +8,25 @@ module webview
 #include <gtk/gtk.h>
 #include <webkit2/webkit2.h>
 
-// struct C.GtkWidget{}
+struct C.GtkWidget{}
 
-fn C.gtk_init()
+fn C.gtk_init(argc int, argv voidptr)
+
+fn C.gtk_window_new() &C.GtkWidget{}
+
+fn C.gtk_window_set_default_size(win C.GtkWidget, w int, h int)
+
+fn C.gtk_widget_show_all(win C.GtkWidget)
+
+fn C.gtk_main()
 
 // fn C.webkit_web_view_new()
 
 fn new_linux_web_view() {
-	C.gtk_init()
+	C.gtk_init(0, voidptr(0))
+	win := C.gtk_window_new(0)
+	C.gtk_window_set_default_size(win, 1000, 600)
+	C.gtk_widget_show_all(win)
+	C.gtk_main()
 	// C.webkit_web_view_new()
 }
