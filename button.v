3979abf24cc2af329a79c889287566516fe0d35a
kahsa <52106048+kahsa@users.noreply.github.com>
2021-01-25 16:30:07 +0900

    button: fix cliced state for color

--- button.v
+++ button.v
@@ -62,7 +62,8 @@ fn (mut b Button) init(parent Layout) {
 		b.image = b.ui.gg.create_image(b.icon_path)
 	}
 	mut subscriber := parent.get_subscriber()
-	subscriber.subscribe_method(events.on_click, btn_click, b)
+	subscriber.subscribe_method(events.on_mouse_down, btn_click, b)
+	subscriber.subscribe_method(events.on_mouse_up, btn_click, b)
 }
 
 pub fn button(c ButtonConfig) &Button {
