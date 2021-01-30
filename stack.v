7c93612b1ae3703a02ab2fdf5a522a826a3d8ea2
Alexander Medvednikov <alexander@vlang.io>
2021-01-30 14:17:12 +0100

    stack: remove old code

--- stack.v
+++ stack.v
@@ -159,63 +159,8 @@ fn (mut s Stack) draw() {
 	for child in s.children {
 		child.draw()
 	}
-	/*
-	per_child_height := if child_len > 0 {
- (s.get_oriented_height() - total_spacing) / child_len } else { 0 }
-	mut pos_y := s.get_oriented_y_axis()
-	if s.vertical_alignment == .bottom {
-		// Move the stack to the bottom. First find the biggest height.
-		_, parent_height := s.parent.size()
-	println('parent_height=$parent_height xxx $s.height')
-		pos_y = parent_height - s.height
-	}
-	mut size_x := 0
-
-	for child in s.children {
-		mut w := 0
-		mut h := 0
-		if s.direction == .column || s.vertical_alignment == .bottom {
-			w, h = child.propose_size(s.width, per_child_height)
-			child.set_pos(s.align(w), pos_y)
-		} else  {//if s.direction == .row {
-			h, w = child.propose_size(per_child_height, s.height)
-			child.set_pos(pos_y, s.align(w))
-		}
-		if w > size_x {
-			size_x = w
-		}
-		child.draw()
-		pos_y += h + s.spacing
-	}
-	if s.stretch {
-		return
-	}
-	pos_y -= s.spacing
-	if s.height == 0 {
-	//s.set_oriented_height(pos_y - s.get_oriented_y_axis())
-	}
-	w := s.get_oriented_width()
-	if w == 0 || w < size_x {
-		s.set_oriented_width(size_x)
-	}
-	*/
 }
 
-/*
-fn (s &Stack) align(size int) int {
-	align := if s.direction == .column {
-		int(s.horizontal_alignment)
-	} else {
-		int(s.vertical_alignment)
-	}
-	match align {
-		0 { return s.get_oriented_x_axis() }
-		1 { return s.get_oriented_x_axis() + ((s.get_oriented_width() - size) / 2) }
-		2 { return (s.get_oriented_x_axis() + s.get_oriented_width()) - size }
-		else { return s.get_oriented_x_axis() }
-	}
-}
-*/
 fn (s &Stack) get_ui() &UI {
 	return s.ui
 }
@@ -252,60 +197,3 @@ fn (s &Stack) is_focused() bool {
 
 fn (s &Stack) resize(width int, height int) {
 }
-
-/*
-// Helpers to correctly get width, height, x, y for both row & column.
-fn (s &Stack) get_oriented_height() int {
-	return if s.direction == .column {
-		s.height
-	} else {
-		s.width
-	}
-}
-
-fn (s &Stack) get_oriented_width() int {
-	return if s.direction == .column {
-		s.width
-	} else {
-		s.height
-	}
-}
-
-fn (s &Stack) get_oriented_y_axis() int {
-	return if s.direction == .column {
-		s.y
-	} else {
-		s.x
-	}
-}
-
-fn (s &Stack) get_oriented_x_axis() int {
-	return if s.direction == .column {
-		s.x
-	} else {
-		s.y
-	}
-}
-
-fn (mut s Stack) set_oriented_height(h int) int {
-	if s.direction == .column {
-		if s.height == 0 {
-			s.height = h
-		}
-	} else {
-		s.width = h
-	}
-	return h
-}
-
-fn (mut s Stack) set_oriented_width(w int) int {
-	if s.direction == .column {
-		s.width = w
-	} else {
-		if s.height == 0 {
-			s.height = w
-		}
-	}
-	return w
-}
-*/
