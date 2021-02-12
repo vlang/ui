513fbd13a80ad13424185c33cf641d250b3f4c45
StunxFS <56417208+StunxFS@users.noreply.github.com>
2021-02-12 02:59:29 -0400

    all: update copyright year (#292)

--- easing.v
+++ easing.v
@@ -1,4 +1,4 @@
-// Copyright (c) 2020 Leah Lundqvist. All rights reserved.
+// Copyright (c) 2020-2021 Leah Lundqvist. All rights reserved.
 // Use of this source code is governed by a GPL license
 // that can be found in the LICENSE file.
 module ui
