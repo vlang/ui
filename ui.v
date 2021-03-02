98cde110d8cbb3470c41ef44bb095b6066d2777f
rcqls <rdrouilh@gmail.com>
2021-03-02 04:28:35 +0100

    ui widget field mutable

--- ui.v
+++ ui.v
@@ -57,7 +57,6 @@ pub interface Widget {
 	x int
 	y int
 	z_index int
-	ui &UI
 	init(Layout)
 	// key_down(KeyEvent)
 	draw()
@@ -70,6 +69,8 @@ pub interface Widget {
 	propose_size(w int, h int) (int, int)
 	size() (int, int)
 	is_focused() bool
+mut:
+	ui &UI
 }
 
 // pub fn iwidget(x Widget) Widget { return x }
