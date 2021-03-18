87d7d281f190b1b821f56a5e888bcbb13d22a97b
spaceface <spaceface7777@gmail.com>
2021-03-18 15:27:32 +0100

    use `mut` when smartcasting mutable interfaces (part 2) (#334)

--- window.v
+++ window.v
@@ -1032,7 +1032,7 @@ pub fn (w &Window) child(from ...int) Widget {
 pub fn (w &Window) update_layout() {
 	// update root_layout
 	mut s := w.root_layout
-	if s is Stack {
+	if mut s is Stack {
 		s.update_all_children_recursively(w)
 	}
 }
