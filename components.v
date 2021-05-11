a4dbcf448e8354bfa2c1bd35caa38f3931c9d064
R cqls <rdrouilh@gmail.com>
2021-05-11 13:26:53 +0200

    rename Component to ComponentChild

--- components.v
+++ components.v
@@ -2,15 +2,15 @@ module ui
 
 //---
 // A Composable Widget is concretely a Layout (Stack, Group or CanvasLayout) to be added to the window children tree (sort of DOM).
-// A Component is the struct gathering:
+// A Component is the struct gathering ComponentChild:
 //  1) the unique composable widget ( i.e this unique root layout)
 //  2) the children of this root layout (i.e. widgets or sub-components). N.B.: the other sub-layouts possibly used in the root layout children tree are not in the component struct.
 // All composable widget and children widgets/sub-components have a unique field `component` corresponding to this Component structure.
-// All members (layout and children) are then all connected.
+// All members (layout and children) are then all connected as ComponentChild having component field.
 // Remark: To become possibly a member of a parent component, a component has to have this field `component` to be connected to
 //---
 
-interface Component {
+interface ComponentChild {
 mut:
 	component voidptr
 }
