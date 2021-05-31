770888df2ebb834c83575aed42c762705b89c0be
R cqls <rdrouilh@gmail.com>
2021-05-31 22:01:48 +0200

    working on dragging for scrollview

--- ui_extra_scrollview.v
+++ ui_extra_scrollview.v
@@ -71,6 +71,14 @@ const (
 	scrollbar_focused_background_color = gx.rgb(255, 0, 0)
 )
 
+enum ScrollMode {
+	view
+	btn_x
+	btn_y
+	bar_x
+	bar_y
+}
+
 // type ScrollViewChangedFn = fn (arg_1 voidptr, arg_2 voidptr)
 
 [heap]
@@ -91,6 +99,8 @@ pub mut:
 	// active scrollbar
 	active_x bool
 	active_y bool
+	// dragging
+	dragging int // 0=invalid, 1=x, 2=y
 	// sizes of widget
 	orig_x     int
 	orig_y     int
@@ -152,10 +162,53 @@ pub fn (sv &ScrollView) is_active() bool {
 	return sv.active_x || sv.active_y
 }
 
-fn (sv &ScrollView) point_inside(x f64, y f64) bool {
-	wx, wy := sv.orig_x + sv.widget.offset_x, sv.orig_y + sv.widget.offset_y
-	// println("scrollview inside ($x, $y) ($wx + $sv.width, $wy + $sv.height)")
-	return x >= wx && x <= wx + sv.width && y >= wy && y <= wy + sv.height
+fn (sv &ScrollView) point_inside(x f64, y f64, mode ScrollMode) bool {
+	mut x_min, mut y_min, mut x_max, mut y_max := 0, 0, 0, 0
+	match mode {
+		.view {
+			x_min, y_min = sv.orig_x + sv.widget.offset_x, sv.orig_y + sv.widget.offset_y
+			x_max, y_max = x_min + sv.width, y_min + sv.height
+		}
+		.bar_x {
+			x_min, y_min = sv.orig_x, sv.orig_y + sv.height - ui.scrollbar_size
+			x_max, y_max = x_min + sv.sb_w, y_min + ui.scrollbar_size
+		}
+		.bar_y {
+			x_min, y_min = sv.orig_x + sv.width - ui.scrollbar_size, sv.orig_y
+			x_max, y_max = x_min + ui.scrollbar_size, y_min + sv.sb_h
+		}
+		.btn_x {
+			x_min, y_min = sv.orig_x + sv.btn_x, sv.orig_y + sv.height - ui.scrollbar_size
+			x_max, y_max = x_min + sv.btn_w, y_min + ui.scrollbar_size
+		}
+		.btn_y {
+			x_min, y_min = sv.orig_x + sv.width - ui.scrollbar_size, sv.orig_y + sv.btn_y
+			x_max, y_max = x_min + ui.scrollbar_size, y_min + sv.btn_h
+		}
+	}
+	return x >= x_min && x <= x_max && y >= y_min && y <= y_max
+}
+
+fn (mut sv ScrollView) change_value(mode ScrollMode) {
+	if mode == .btn_x {
+		if sv.offset_x > 0 {
+			sv.offset_x = 0
+		}
+		min_offset_x := -(sv.adj_width - sv.width + 2 * ui.scrollbar_size)
+		if sv.offset_x < min_offset_x {
+			sv.offset_x = min_offset_x
+		}
+		sv.btn_x = int(f32(sv.offset_x) * f32(sv.sb_w - sv.btn_w) / f32(min_offset_x))
+	} else if mode == .btn_y {
+		if sv.offset_y > 0 {
+			sv.offset_y = 0
+		}
+		min_offset_y := -(sv.adj_height - sv.height + 2 * ui.scrollbar_size)
+		if sv.offset_y < min_offset_y {
+			sv.offset_y = min_offset_y
+		}
+		sv.btn_y = int(f32(sv.offset_y) * f32(sv.sb_h - sv.btn_h) / f32(min_offset_y))
+	}
 }
 
 pub fn (mut sv ScrollView) clip() {
@@ -195,37 +248,98 @@ pub fn (sv &ScrollView) draw() {
 }
 
 fn scrollview_scroll(mut sv ScrollView, e &ScrollEvent, zzz voidptr) {
-	if sv.is_active() && sv.point_inside(e.mouse_x, e.mouse_y) {
+	if sv.is_active() && sv.point_inside(e.mouse_x, e.mouse_y, .view) {
 		if sv.active_x {
 			sv.offset_x += int(e.x * 3) * 3
-			if sv.offset_x > 0 {
-				sv.offset_x = 0
-			}
-			min_offset_x := -(sv.adj_width - sv.width + 2 * ui.scrollbar_size)
-			if sv.offset_x < min_offset_x {
-				sv.offset_x = min_offset_x
-			}
-			sv.btn_x = int(f32(sv.offset_x) * f32(sv.sb_w - sv.btn_w) / f32(min_offset_x))
+			// if sv.offset_x > 0 {
+			// 	sv.offset_x = 0
+			// }
+			// min_offset_x := -(sv.adj_width - sv.width + 2 * ui.scrollbar_size)
+			// if sv.offset_x < min_offset_x {
+			// 	sv.offset_x = min_offset_x
+			// }
+			// sv.btn_x = int(f32(sv.offset_x) * f32(sv.sb_w - sv.btn_w) / f32(min_offset_x))
+			sv.change_value(.btn_x)
 		}
 
 		if sv.active_y {
 			sv.offset_y += int(e.y * 3) * 3
-			if sv.offset_y > 0 {
-				sv.offset_y = 0
-			}
-			min_offset_y := -(sv.adj_height - sv.height + 2 * ui.scrollbar_size)
-			if sv.offset_y < min_offset_y {
-				sv.offset_y = min_offset_y
-			}
-			sv.btn_y = int(f32(sv.offset_y) * f32(sv.sb_h - sv.btn_h) / f32(min_offset_y))
+			// if sv.offset_y > 0 {
+			// 	sv.offset_y = 0
+			// }
+			// min_offset_y := -(sv.adj_height - sv.height + 2 * ui.scrollbar_size)
+			// if sv.offset_y < min_offset_y {
+			// 	sv.offset_y = min_offset_y
+			// }
+			// sv.btn_y = int(f32(sv.offset_y) * f32(sv.sb_h - sv.btn_h) / f32(min_offset_y))
+			sv.change_value(.btn_y)
 		}
 	}
 }
 
-fn scrollview_key_down(mut s ScrollView, e &KeyEvent, zzz voidptr) {
+fn scrollview_click(mut s ScrollView, e &MouseEvent, zzz voidptr) {
 	// if s.hidden {
 	// 	return
 	// }
+	// if !s.point_inside_thumb(e.x, e.y) && (!s.point_inside(e.x, e.y) || s.focus_on_thumb_only) {
+	// 	s.is_focused = false
+	// 	return
+	// }
+	// if !s.focus_on_thumb_only {
+	// 	s.change_value(e.x, e.y)
+	// }
+	// s.is_focused = true
+}
+
+fn scrollview_touch_move(mut sv ScrollView, e &MouseMoveEvent, zzz voidptr) {
+	if !sv.is_active() {
+		return
+	}
+
+	// if s.entering && s.point_inside_thumb(e.x, e.y) {
+	// 	// println('scrollview touch move DRAGGING')
+	// 	s.dragging = true
+	// }
+	// if s.dragging {
+	// 	s.change_value(int(e.x), int(e.y))
+	// }
+}
+
+fn scrollview_mouse_down(mut sv ScrollView, e &MouseEvent, zzz voidptr) {
+	if !sv.is_active() {
+		return
+	}
+	if int(e.button) == 0 {
+		for mode in [1, 2] {
+			if sv.point_inside(e.x, e.y, ScrollMode(mode)) {
+				sv.dragging = mode // 1=x or 2=y
+				break
+			}
+		}
+	}
+}
+
+fn scrollview_mouse_up(mut sv ScrollView, e &MouseEvent, zzz voidptr) {
+	if !sv.is_active() {
+		return
+	}
+	sv.dragging = 0 // invalid neither x nor y
+}
+
+fn scrollview_mouse_move(mut sv ScrollView, e &MouseMoveEvent, zzz voidptr) {
+	if !sv.is_active() {
+		return
+	}
+	if !sv.ui.btn_down[0] { // int(e.mouse_button) == 0 {
+		sv.dragging = 0 // invalid neither x nor y
+	}
+	// sv.change_value()
+}
+
+fn scrollview_key_down(mut sv ScrollView, e &KeyEvent, zzz voidptr) {
+	if !sv.is_active() {
+		return
+	}
 	// if !s.is_focused {
 	// 	return
 	// }
@@ -261,157 +375,8 @@ fn scrollview_key_down(mut s ScrollView, e &KeyEvent, zzz voidptr) {
 	// }
 }
 
-fn scrollview_click(mut s ScrollView, e &MouseEvent, zzz voidptr) {
-	// if s.hidden {
-	// 	return
-	// }
-	// if !s.point_inside_thumb(e.x, e.y) && (!s.point_inside(e.x, e.y) || s.focus_on_thumb_only) {
-	// 	s.is_focused = false
-	// 	return
-	// }
-	// if !s.focus_on_thumb_only {
-	// 	s.change_value(e.x, e.y)
-	// }
-	// s.is_focused = true
-}
-
-fn scrollview_touch_move(mut s ScrollView, e &MouseMoveEvent, zzz voidptr) {
-	// if s.hidden {
-	// 	return
-	// }
-	// if s.entering && s.point_inside_thumb(e.x, e.y) {
-	// 	// println('scrollview touch move DRAGGING')
-	// 	s.dragging = true
-	// }
-	// if s.dragging {
-	// 	s.change_value(int(e.x), int(e.y))
-	// }
-}
-
-fn scrollview_mouse_down(mut s ScrollView, e &MouseEvent, zzz voidptr) {
-	// if s.hidden {
-	// 	return
-	// }
-	// // println('scrollview touchup  NO MORE DRAGGING')
-	// if s.point_inside_thumb(e.x, e.y) {
-	// 	// println('scrollview touch move DRAGGING')
-	// 	s.dragging = true
-	// }
-}
-
-fn scrollview_mouse_up(mut s ScrollView, e &MouseEvent, zzz voidptr) {
-	// // println('scrollview touchup  NO MORE DRAGGING')
-	// s.dragging = false
-}
-
-fn scrollview_mouse_move(mut s ScrollView, e &MouseMoveEvent, zzz voidptr) {
-	// println("scrollview: $s.dragging ${e.mouse_button} ${int(e.mouse_button)}")
-	// if int(e.mouse_button) == 0 {
-	// 	// left: 0, right: 1, middle: 2
-	// 	if s.entering && s.point_inside_thumb(e.x, e.y) {
-	// 		// println("scrollview DRAGGING")
-	// 		s.dragging = true
-	// 	}
-	// } else {
-	// 	s.dragging = false
-	// }
-	// if s.dragging {
-	// 	s.change_value(int(e.x), int(e.y))
-	// }
-}
-
 /*
-fn (s &ScrollView) draw_thumb() {
-	axis := if s.orientation == .horizontal { s.x } else { s.y }
-	rev_axis := if s.orientation == .horizontal { s.y } else { s.x }
-	rev_dim := if s.orientation == .horizontal { s.height } else { s.width }
-	rev_thumb_dim := if s.orientation == .horizontal { s.thumb_height } else { s.thumb_width }
-	dim := if s.orientation == .horizontal { s.width } else { s.height }
-	mut pos := f32(dim) * ((s.val - f32(s.min)) / f32(s.max - s.min))
-	if s.rev_min_max_pos {
-		pos = -pos + f32(dim)
-	}
-	pos += f32(axis)
-	if pos > axis + dim {
-		pos = f32(dim) + f32(axis)
-	}
-	if pos < axis {
-		pos = f32(axis)
-	}
-	middle := f32(rev_axis) - (f32(rev_thumb_dim - rev_dim) / 2)
-	if s.orientation == .horizontal {
-		s.ui.gg.draw_rect(pos - f32(s.thumb_width) / 2, middle, s.thumb_width, s.thumb_height,
-			ui.thumb_color)
-	} else {
-		s.ui.gg.draw_rect(middle, pos - f32(s.thumb_height) / 2, s.thumb_width, s.thumb_height,
-			ui.thumb_color)
-	}
-}
-
-fn (mut s ScrollView) set_pos(x int, y int) {
-	s.x = x
-	s.y = y
-}
-
-fn (mut s ScrollView) set_thumb_size() {
-	if !s.thumb_in_track {
-		s.thumb_height = if s.orientation == .horizontal { s.height + 10 } else { 10 }
-		s.thumb_width = if s.orientation == .horizontal { 10 } else { s.width + 10 }
-	} else {
-		s.thumb_height = if s.orientation == .horizontal { s.height - 3 } else { 10 }
-		s.thumb_width = if s.orientation == .horizontal { 10 } else { s.width - 3 }
-	}
-}
-
-fn (mut s ScrollView) size() (int, int) {
-	if s.orientation == .horizontal {
-		return s.width, s.thumb_height
-	} else {
-		return s.thumb_width, s.height
-	}
-}
-
-fn (mut s ScrollView) propose_size(w int, h int) (int, int) {
-	// TODO: fix
-	$if debug_scrollview ? {
-		println('scrollview propose_size: ($s.width,$s.height) -> ($w, $h) | s.orientation: $s.orientation')
-	}
-	if s.orientation == .horizontal {
-		s.width = w
-	} else {
-		s.height = h
-	}
-	s.set_thumb_size()
-	return s.size()
-}
-
-fn (mut s ScrollView) draw() {
-	offset_start(mut s)
-	// Draw the track
-	s.ui.gg.draw_rect(s.x, s.y, s.width, s.height, ui.scrollview_background_color)
-	if s.track_line_displayed {
-		if s.orientation == .horizontal {
-			s.ui.gg.draw_line(s.x + 2, s.y + s.height / 2, s.x + s.width - 4, s.y + s.height / 2,
-				gx.rgb(0, 0, 0))
-		} else {
-			s.ui.gg.draw_line(s.x + s.width / 2, s.y + 2, s.x + s.width / 2, s.y + s.height - 4,
-				gx.rgb(0, 0, 0))
-		}
-	}
-	if !s.is_focused {
-		s.ui.gg.draw_empty_rect(s.x, s.y, s.width, s.height, ui.scrollview_background_border_color)
-	} else {
-		s.ui.gg.draw_empty_rect(s.x, s.y, s.width, s.height, ui.scrollview_focused_background_border_color)
-	}
-	// Draw the thumb
-	s.draw_thumb()
-	$if bb ? {
-		draw_bb(s, s.ui)
-	}
-	offset_end(mut s)
-}
-
-fn scrollview_key_down(mut s ScrollView, e &KeyEvent, zzz voidptr) {
+fn slider_key_down(mut s Slider, e &KeyEvent, zzz voidptr) {
 	if s.hidden {
 		return
 	}
@@ -450,11 +415,7 @@ fn scrollview_key_down(mut s ScrollView, e &KeyEvent, zzz voidptr) {
 	}
 }
 
-fn (s &ScrollView) point_inside(x f64, y f64) bool {
-	return point_inside<scrollview>(s, x, y) // x >= s.x && x <= s.x + s.width && y >= s.y && y <= s.y + s.height
-}
-
-fn scrollview_click(mut s ScrollView, e &MouseEvent, zzz voidptr) {
+fn slider_click(mut s Slider, e &MouseEvent, zzz voidptr) {
 	if s.hidden {
 		return
 	}
@@ -468,12 +429,12 @@ fn scrollview_click(mut s ScrollView, e &MouseEvent, zzz voidptr) {
 	s.is_focused = true
 }
 
-fn scrollview_touch_move(mut s ScrollView, e &MouseMoveEvent, zzz voidptr) {
+fn slider_touch_move(mut s Slider, e &MouseMoveEvent, zzz voidptr) {
 	if s.hidden {
 		return
 	}
 	if s.entering && s.point_inside_thumb(e.x, e.y) {
-		// println('scrollview touch move DRAGGING')
+		// println('slider touch move DRAGGING')
 		s.dragging = true
 	}
 	if s.dragging {
@@ -481,28 +442,28 @@ fn scrollview_touch_move(mut s ScrollView, e &MouseMoveEvent, zzz voidptr) {
 	}
 }
 
-fn scrollview_mouse_down(mut s ScrollView, e &MouseEvent, zzz voidptr) {
+fn slider_mouse_down(mut s Slider, e &MouseEvent, zzz voidptr) {
 	if s.hidden {
 		return
 	}
-	// println('scrollview touchup  NO MORE DRAGGING')
-	if s.point_inside_thumb(e.x, e.y) {
-		// println('scrollview touch move DRAGGING')
+	// println('slider touchup  NO MORE DRAGGING')
+	if int(e.button) == 0 && s.point_inside_thumb(e.x, e.y) {
+		// println('slider touch move DRAGGING ${e.button}')
 		s.dragging = true
 	}
 }
 
-fn scrollview_mouse_up(mut s ScrollView, e &MouseEvent, zzz voidptr) {
-	// println('scrollview touchup  NO MORE DRAGGING')
+fn slider_mouse_up(mut s Slider, e &MouseEvent, zzz voidptr) {
+	// println('slider touchup  NO MORE DRAGGING')
 	s.dragging = false
 }
 
-fn scrollview_mouse_move(mut s ScrollView, e &MouseMoveEvent, zzz voidptr) {
-	// println("scrollview: $s.dragging ${e.mouse_button} ${int(e.mouse_button)}")
-	if int(e.mouse_button) == 0 {
+fn slider_mouse_move(mut s Slider, e &MouseMoveEvent, zzz voidptr) {
+	// println("slider: $s.dragging ${e.mouse_button} ${int(e.mouse_button)}")
+	if s.ui.btn_down[0] { // int(e.mouse_button) == 0 {
 		// left: 0, right: 1, middle: 2
 		if s.entering && s.point_inside_thumb(e.x, e.y) {
-			// println("scrollview DRAGGING")
+			// println("slider DRAGGING")
 			s.dragging = true
 		}
 	} else {
@@ -512,93 +473,4 @@ fn scrollview_mouse_move(mut s ScrollView, e &MouseMoveEvent, zzz voidptr) {
 		s.change_value(int(e.x), int(e.y))
 	}
 }
-
-fn (mut s ScrollView) change_value(x int, y int) {
-	dim := if s.orientation == .horizontal { s.width } else { s.height }
-	axis := if s.orientation == .horizontal { s.x } else { s.y }
-	// TODO parser bug ` - axis`
-	mut pos := if s.orientation == .horizontal { x } else { y }
-	pos -= axis
-	if s.rev_min_max_pos {
-		pos = -pos + dim
-	}
-	s.val = f32(s.min) + (f32(pos) * f32(s.max - s.min)) / f32(dim)
-	if int(s.val) < s.min {
-		s.val = f32(s.min)
-	} else if int(s.val) > s.max {
-		s.val = f32(s.max)
-	}
-	if s.on_value_changed != voidptr(0) {
-		parent := s.parent
-		state := parent.get_state()
-		s.on_value_changed(state, s)
-	}
-}
-
-fn (mut s ScrollView) set_visible(state bool) {
-	s.hidden = state
-}
-
-fn (mut s ScrollView) focus() {
-	parent := s.parent
-	parent.unfocus_all()
-	s.is_focused = true
-}
-
-fn (s &ScrollView) is_focused() bool {
-	return s.is_focused
-}
-
-fn (mut s ScrollView) unfocus() {
-	s.is_focused = false
-}
-
-fn (s &ScrollView) point_inside_thumb(x f64, y f64) bool {
-	sx, sy := s.x + s.offset_x, s.y + s.offset_y
-	axis := if s.orientation == .horizontal { sx } else { sy }
-	rev_axis := if s.orientation == .horizontal { sy } else { sx }
-	rev_dim := if s.orientation == .horizontal { s.height } else { s.width }
-	rev_thumb_dim := if s.orientation == .horizontal { s.thumb_height } else { s.thumb_width }
-	dim := if s.orientation == .horizontal { s.width } else { s.height }
-	mut pos := f32(dim) * ((s.val - f32(s.min)) / f32(s.max - s.min))
-	if s.rev_min_max_pos {
-		pos = -pos + f32(dim)
-	}
-	pos += f32(axis)
-	if pos > axis + dim {
-		pos = f32(dim) + f32(axis)
-	}
-	if pos < axis {
-		pos = f32(axis)
-	}
-	middle := f32(rev_axis) - (f32(rev_thumb_dim - rev_dim) / 2)
-	$if android {
-		tol := 20.
-		if s.orientation == .horizontal {
-			t_x := pos - f32(s.thumb_width) / 2 - tol
-			t_y := middle - tol
-			return x >= t_x && x <= t_x + f32(s.thumb_width) + tol * 2 && y >= t_y
-				&& y <= t_y + f32(s.thumb_height) + tol * 2
-		} else {
-			t_x := middle - tol
-			t_y := pos - f32(s.thumb_height) / 2 - tol
-			// println('scrollview inside: $x >= $t_x && $x <= ${t_x + f32(s.thumb_width)} && $y >= $t_y && $y <= ${
-			// 	t_y + f32(s.thumb_height)}')
-			return x >= t_x && x <= t_x + f32(s.thumb_width) + tol * 2 && y >= t_y
-				&& y <= t_y + f32(s.thumb_height) + tol * 2
-		}
-	} $else {
-		if s.orientation == .horizontal {
-			t_x := pos - f32(s.thumb_width) / 2
-			t_y := middle
-			return x >= t_x && x <= t_x + f32(s.thumb_width) && y >= t_y
-				&& y <= t_y + f32(s.thumb_height)
-		} else {
-			t_x := middle
-			t_y := pos - f32(s.thumb_height) / 2
-			// println("scrollview inside: $x >= $t_x && $x <= ${t_x + f32(s.thumb_width)} && $y >= $t_y && $y <= ${t_y + f32(s.thumb_height)}")
-			return x >= t_x && x <= t_x + f32(s.thumb_width) && y >= t_y
-				&& y <= t_y + f32(s.thumb_height)
-		}
-	}
-}*/
+*/
