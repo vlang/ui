cd7b50c2b5a7a2dbad6163f6eff8baf8d91c8e3f
R cqls <rdrouilh@gmail.com>
2021-08-19 15:40:30 +0200

    vfmt

--- ui_extra_scrollview.v
+++ ui_extra_scrollview.v
@@ -50,7 +50,8 @@ enum ScrollViewPart {
 	bar
 }
 
-type ScrollViewChangedFn = fn (voidptr)
+type ScrollViewChangedFn = fn (sw ScrollableWidget)
+// WORKAROUND: type ScrollViewChangedFn = fn (sw voidptr)
 
 interface ScrollableWidget {
 mut:
@@ -478,12 +479,14 @@ fn (mut sv ScrollView) change_value(mode ScrollViewPart) {
 	}
 	// User defined treatment for scrollable widget
 	if sw.on_scroll_change != ScrollViewChangedFn(0) {
-		if mut sw is TextBox {
-			sw.on_scroll_change(sw)
-		} else if mut sw is ListBox {
-			sw.on_scroll_change(sw)
-		} else {
-		}
+		sw.on_scroll_change(sw)
+		// WORKAROUND: 
+		// if mut sw is TextBox {
+		// 	sw.on_scroll_change(sw)
+		// } else if mut sw is ListBox {
+		// 	sw.on_scroll_change(sw)
+		// } else {
+		// }
 	}
 }
 
