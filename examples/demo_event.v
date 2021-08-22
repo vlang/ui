89855b665da8ce52a92e79624610c8188162f42f
R cqls <rdrouilh@gmail.com>
2021-08-22 05:51:29 +0200

    add example demo_event.v

---
+++ examples/demo_event.v
@@ -0,0 +1,45 @@
+import ui
+// import gg
+import gx
+
+struct App {
+mut:
+	window &ui.Window = 0
+	info   string     = '....'
+}
+
+fn main() {
+	mut app := &App{}
+	app.window = ui.window(
+		state: app
+		width: 600
+		height: 600
+		title: 'V UI: Rectangles'
+		mode: .resizable
+		on_key_down: fn (e ui.KeyEvent, w &ui.Window) {
+			mut tb := w.textbox('info')
+			tb.set_text('key_down:\n$e')
+		}
+		on_char: fn (e ui.KeyEvent, w &ui.Window) {
+			mut tb := w.textbox('info')
+			s := utf32_to_str(e.codepoint)
+			tb.set_text('${*tb.text} \nchar: <$s>\n$e')
+		}
+		children: [
+			ui.row(
+				widths: ui.stretch
+				heights: ui.stretch
+				children: [
+					ui.textbox(
+						id: 'info'
+						mode: .multiline | .read_only
+						bg_color: gx.hex(0xfcf4e4ff)
+						text: &app.info
+						text_size: 24
+					),
+				]
+			),
+		]
+	)
+	ui.run(app.window)
+}
