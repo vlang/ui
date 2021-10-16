3cd2adfc7cc3d7a10b3363d547254448c51afbd6
R cqls <rdrouilh@gmail.com>
2021-10-16 23:05:16 +0200

    more on settings

--- ui_settings.v
+++ ui_settings.v
@@ -25,93 +25,93 @@ pub mut:
 	colors_  map[string][]gx.Color
 }
 
-fn (c &SettingsUI) int(k ...string) int {
+pub fn (c &SettingsUI) int(k ...string) int {
 	return c.int_[k.join('.')]
 }
 
-fn (mut c SettingsUI) int_for(val int, k ...string) {
+pub fn (mut c SettingsUI) int_for(val int, k ...string) {
 	c.int_[k.join('.')] = val
 }
 
-fn (c &SettingsUI) ints(k ...string) []int {
+pub fn (c &SettingsUI) ints(k ...string) []int {
 	return c.ints_[k.join('.')]
 }
 
-fn (mut c SettingsUI) ints_for(val []int, k ...string) {
+pub fn (mut c SettingsUI) ints_for(val []int, k ...string) {
 	c.ints_[k.join('.')] = val
 }
 
-fn (c &SettingsUI) f32(k ...string) f32 {
+pub fn (c &SettingsUI) f32(k ...string) f32 {
 	return c.f32_[k.join('.')]
 }
 
-fn (c &SettingsUI) f32s(k ...string) []f32 {
+pub fn (c &SettingsUI) f32s(k ...string) []f32 {
 	return c.f32s_[k.join('.')]
 }
 
-fn (mut c SettingsUI) f32_for(val f32, k ...string) {
+pub fn (mut c SettingsUI) f32_for(val f32, k ...string) {
 	c.f32_[k.join('.')] = val
 }
 
-fn (mut c SettingsUI) f32s_for(val []f32, k ...string) {
+pub fn (mut c SettingsUI) f32s_for(val []f32, k ...string) {
 	c.f32s_[k.join('.')] = val
 }
 
-fn (c &SettingsUI) bool(k ...string) bool {
+pub fn (c &SettingsUI) bool(k ...string) bool {
 	return c.bool_[k.join('.')]
 }
 
-fn (mut c SettingsUI) bool_for(val bool, k ...string) {
+pub fn (mut c SettingsUI) bool_for(val bool, k ...string) {
 	c.bool_[k.join('.')] = val
 }
 
-fn (c &SettingsUI) bools(k ...string) []bool {
+pub fn (c &SettingsUI) bools(k ...string) []bool {
 	return c.bools_[k.join('.')]
 }
 
-fn (mut c SettingsUI) bools_for(val []bool, k ...string) {
+pub fn (mut c SettingsUI) bools_for(val []bool, k ...string) {
 	c.bools_[k.join('.')] = val
 }
 
-fn (c &SettingsUI) string(k ...string) string {
+pub fn (c &SettingsUI) string(k ...string) string {
 	return c.string_[k.join('.')]
 }
 
-fn (mut c SettingsUI) string_for(val string, k ...string) {
+pub fn (mut c SettingsUI) string_for(val string, k ...string) {
 	c.string_[k.join('.')] = val
 }
 
-fn (c &SettingsUI) strings(k ...string) []string {
+pub fn (c &SettingsUI) strings(k ...string) []string {
 	return c.strings_[k.join('.')]
 }
 
-fn (mut c SettingsUI) strings_for(val []string, k ...string) {
+pub fn (mut c SettingsUI) strings_for(val []string, k ...string) {
 	c.strings_[k.join('.')] = val
 }
 
-fn (c &SettingsUI) color(k ...string) gx.Color {
+pub fn (c &SettingsUI) color(k ...string) gx.Color {
 	return c.color_[k.join('.')]
 }
 
-fn (mut c SettingsUI) color_for(val gx.Color, k ...string) {
+pub fn (mut c SettingsUI) color_for(val gx.Color, k ...string) {
 	c.color_[k.join('.')] = val
 }
 
-fn (c &SettingsUI) colors(k ...string) []gx.Color {
+pub fn (c &SettingsUI) colors(k ...string) []gx.Color {
 	return c.colors_[k.join('.')]
 }
 
-fn (mut c SettingsUI) colors_for(val []gx.Color, k ...string) {
+pub fn (mut c SettingsUI) colors_for(val []gx.Color, k ...string) {
 	c.colors_[k.join('.')] = val
 }
 
-fn load_settings(path string) ?SettingsUI {
+pub fn load_settings(path string) ?SettingsUI {
 	raw := os.read_file(path) ?
 	settings := json.decode(SettingsUI, raw) ?
 	return settings
 }
 
-fn save_settings(path string, s SettingsUI) ? {
+pub fn save_settings(path string, s SettingsUI) ? {
 	settings := json.encode_pretty(s)
 	os.write_file(path, settings) ?
 }
@@ -129,7 +129,3 @@ pub fn (mut w Window) save_settings() {
 	}
 	save_settings(ui.settings_path, w.settings) or { panic(err) }
 }
-
-/*
-styles, font
-*/
