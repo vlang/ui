b052a9effb0ba71f7b73b945c46ad37b60d9df8f
Alexander Medvednikov <alexander@vlang.io>
2021-10-17 07:17:41 +0300

    child window example

---
+++ examples/child_window.v
@@ -0,0 +1,48 @@
+import ui
+
+const (
+	win_width  = 450
+	win_height = 120
+)
+
+struct App {
+mut:
+	window         &ui.Window = 0
+	title_box_text string
+}
+
+fn main() {
+	mut app := &App{}
+	app.window = ui.window(
+		width: win_width
+		height: win_height
+		title: 'Child window'
+		state: app
+		children: [
+			ui.column(
+				children: [
+					ui.button(text: 'Create a window', onclick: btn_click, width: 150),
+					ui.textbox(placeholder: 'Test textbox'),
+				]
+			),
+		]
+	)
+	ui.run(app.window)
+}
+
+fn btn_click(mut app App, btn &ui.Button) {
+	app.window.child_window(
+		children: [
+			ui.column(
+				children: [
+					ui.textbox(placeholder: 'Name'),
+					ui.button(text: 'Greet me', onclick: btn_greet_click, width: 150),
+				]
+			),
+		]
+	)
+}
+
+fn btn_greet_click(mut app App, btn &ui.Button) {
+	ui.message_box('Hello!')
+}
