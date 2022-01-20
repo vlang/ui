201be0fd2e237ce6da592824a3bedef5864b4e56
kahsa <kahsa@moon.gmobb.jp>
2022-01-21 04:39:57 +0900

    update copyright 2022 (#417)

--- window.v
+++ window.v
@@ -1,4 +1,4 @@
-// Copyright (c) 2020-2021 Alexander Medvednikov. All rights reserved.
+// Copyright (c) 2020-2022 Alexander Medvednikov. All rights reserved.
 // Use of this source code is governed by a GPL license that can be found in the LICENSE file.
 module ui
 
