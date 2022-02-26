e7ee5b613896b028abf2e2a726367284ed1d2dd9
R cqls <rdrouilh@gmail.com>
2022-02-26 11:47:18 +0100

    Adding c extension for highlighting (just a copy paste of v syntax)

--- extra_text_syntax.v
+++ extra_text_syntax.v
@@ -10,6 +10,14 @@ struct Chunk {
 
 type SyntaxStyle = map[string]gx.Color
 
+type SyntaxMapBool = map[string]bool
+
+type SyntaxMapStrings = map[string][]string
+
+type SyntaxMapArrayStrings = map[string][][]string
+
+type SyntaxMapRunes = map[string][]rune
+
 [heap]
 struct SyntaxHighLighter {
 mut:
@@ -17,12 +25,13 @@ mut:
 	ustr             []rune
 	chunks           map[string][]Chunk
 	lang             string
+	lang_exts        SyntaxMapStrings
 	langs            map[string]SyntaxStyle
-	is_multiline     map[string]bool
-	keywords         map[string][]string
-	singleline       map[string][]string
-	multiline        map[string][][]string
-	between_one_rune map[string][]rune
+	is_multiline     map[string]SyntaxMapBool
+	keywords         map[string]SyntaxMapStrings
+	singleline       map[string]SyntaxMapStrings
+	multiline        map[string]SyntaxMapArrayStrings
+	between_one_rune map[string]SyntaxMapRunes
 	// for loop
 	i     int
 	j     int
@@ -35,18 +44,36 @@ fn syntaxhighlighter() &SyntaxHighLighter {
 }
 
 fn (mut sh SyntaxHighLighter) init(tv &TextView) {
-	sh.load_v()
-	sh.set_lang('')
 	unsafe {
 		sh.tv = tv
 	}
+	// stuff for highlighting features
+	sh.langs = {}
+	sh.keywords = {}
+	sh.singleline = {}
+	sh.multiline = {}
+	sh.between_one_rune = {}
+	sh.is_multiline = {}
+	// load language
+	sh.lang_exts = {
+		'v': ['.v', '.vv', '.vsh']
+		'c': ['.h', '.c']
+	}
+	sh.load_v()
+	sh.load_c()
+	sh.set_lang('')
 }
 
 pub fn (mut sh SyntaxHighLighter) set_lang(ext string) {
-	sh.lang = if ext.len > 0 && ext[0..1] == '.' {
-		if ext[1..] in sh.langs.keys() { ext[1..] } else { '' }
-	} else {
-		''
+	sh.lang = ''
+	if ext.len == 0 {
+		return
+	}
+	for lang, exts in sh.lang_exts {
+		if ext in exts {
+			sh.lang = lang
+			return
+		}
 	}
 }
 
@@ -55,7 +82,7 @@ fn (sh &SyntaxHighLighter) is_lang_loaded() bool {
 }
 
 fn (mut sh SyntaxHighLighter) load_v() {
-	sh.keywords = {
+	sh.keywords['v'] = {
 		'types':   'int|i8|i16|i64|i128|u8|u16|u32|u64|u128|f32|f64|bool|byte|byteptr|charptr|voidptr|string|ustring|rune'.split('|')
 		'decl':    'mut|pub|unsafe|default|module|import|const|interface'.split('|')
 		'control': (
@@ -63,13 +90,12 @@ fn (mut sh SyntaxHighLighter) load_v() {
 			'|$' + 'if|$' + 'else').split('|')
 		'keyword': 'fn|type|enum|struct|union|interface|map|assert|sizeof|typeof|__offsetof'.split('|')
 	}
-	sh.singleline = {
+	sh.singleline['v'] = {
 		'comment': ['//', '#']
 	}
-	sh.multiline = {
+	sh.multiline['v'] = {
 		'comment': [['/*', '*/']]
 	}
-	sh.langs = {}
 	sh.langs['v'] = {
 		'comment': gx.gray
 		'keyword': gx.blue
@@ -78,11 +104,42 @@ fn (mut sh SyntaxHighLighter) load_v() {
 		'types':   gx.purple
 		'string':  gx.dark_green
 	}
-	sh.is_multiline = {
+	sh.between_one_rune['v'] = {
+		'string': [`'`, `"`, `\``]
+	}
+	sh.is_multiline['v'] = {
 		'/*': false
 	}
-	sh.between_one_rune = {
+}
+
+fn (mut sh SyntaxHighLighter) load_c() {
+	sh.keywords['c'] = {
+		'types':   'int|i8|i16|i64|i128|u8|u16|u32|u64|u128|f32|f64|bool|byte|byteptr|charptr|voidptr|string|ustring|rune'.split('|')
+		'decl':    'mut|pub|unsafe|default|module|import|const|interface'.split('|')
+		'control': (
+			'enum|in|is|or|as|in|is|or|break|continue|match|if|else|for|go|goto|defer|return|shared|select|rlock|lock|atomic|asm' +
+			'|$' + 'if|$' + 'else').split('|')
+		'keyword': 'fn|type|enum|struct|union|interface|map|assert|sizeof|typeof|__offsetof'.split('|')
+	}
+	sh.singleline['c'] = {
+		'comment': ['//', '#']
+	}
+	sh.multiline['c'] = {
+		'comment': [['/*', '*/']]
+	}
+	sh.langs['c'] = {
+		'comment': gx.gray
+		'keyword': gx.blue
+		'control': gx.orange
+		'decl':    gx.red
+		'types':   gx.purple
+		'string':  gx.dark_green
+	}
+	sh.between_one_rune['c'] = {
 		'string': [`'`, `"`, `\``]
+	}
+	sh.is_multiline['c'] = {
+		'/*': false
 	}
 }
 
@@ -90,19 +147,18 @@ fn (mut sh SyntaxHighLighter) parse_chunks(j int, y int, line string) {
 	if !sh.is_lang_loaded() {
 		return
 	}
-
 	sh.j, sh.y = j, y
 
 	if j == 0 {
-		for k, _ in sh.is_multiline {
-			sh.is_multiline[k] = false
+		for k, _ in sh.is_multiline[sh.lang] {
+			sh.is_multiline[sh.lang][k] = false
 		}
 	}
 
 	sh.ustr = line.runes()
 	l := line.trim_space()
 	// single line comment
-	for typ, vals in sh.singleline {
+	for typ, vals in sh.singleline[sh.lang] {
 		for val in vals {
 			if sh.parse_chunk_oneline_comment(typ, val, l) {
 				return
@@ -111,7 +167,7 @@ fn (mut sh SyntaxHighLighter) parse_chunks(j int, y int, line string) {
 	}
 
 	// multilines or single line
-	for typ, vals in sh.multiline {
+	for typ, vals in sh.multiline[sh.lang] {
 		for val in vals {
 			if sh.parse_chunk_multiline_comment(typ, val[0], val[1], l) {
 				return
@@ -124,13 +180,13 @@ fn (mut sh SyntaxHighLighter) parse_chunks(j int, y int, line string) {
 	for sh.i < sh.ustr.len {
 		sh.start = sh.i
 		// String or betwwen one same rune
-		for typ, vals in sh.between_one_rune {
+		for typ, vals in sh.between_one_rune[sh.lang] {
 			for val in vals {
 				sh.parse_chunk_between_one_rune(typ, val)
 			}
 		}
 		// Keyword
-		for keyword, _ in sh.keywords {
+		for keyword, _ in sh.keywords[sh.lang] {
 			sh.parse_chunk_keyword(keyword)
 		}
 		sh.i++
@@ -149,17 +205,17 @@ fn (mut sh SyntaxHighLighter) parse_chunk_oneline_comment(typ string, comment_se
 
 fn (mut sh SyntaxHighLighter) parse_chunk_multiline_comment(typ string, comment_start string, comment_stop string, line_trim string) bool {
 	if line_trim.starts_with(comment_start) {
-		sh.is_multiline[comment_start] = !line_trim.ends_with(comment_stop)
+		sh.is_multiline[sh.lang][comment_start] = !line_trim.ends_with(comment_stop)
 		sh.add_chunk(typ, sh.y, 0, sh.ustr.len)
 		return true
 	}
-	if sh.is_multiline[comment_start] && !line_trim.contains(comment_stop) {
+	if sh.is_multiline[sh.lang][comment_start] && !line_trim.contains(comment_stop) {
 		sh.add_chunk(typ, sh.y, 0, sh.ustr.len)
 		return true
 	}
-	if sh.is_multiline[comment_start] && line_trim.contains(comment_stop)
+	if sh.is_multiline[sh.lang][comment_start] && line_trim.contains(comment_stop)
 		&& line_trim.ends_with(comment_stop) {
-		sh.is_multiline[comment_start] = false
+		sh.is_multiline[sh.lang][comment_start] = false
 		sh.add_chunk(typ, sh.y, 0, sh.ustr.len)
 		return true
 	}
@@ -184,7 +240,7 @@ fn (mut sh SyntaxHighLighter) parse_chunk_keyword(typ string) {
 		sh.i++
 	}
 	word := sh.ustr[sh.start..sh.i].string()
-	if word in sh.keywords[typ] {
+	if word in sh.keywords[sh.lang][typ] {
 		sh.add_chunk(typ, sh.y, sh.start, sh.i)
 	}
 }
