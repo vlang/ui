2a6839966c44e31f98101b854c15d843f4f5a42b
R cqls <rdrouilh@gmail.com>
2022-07-12 11:36:18 +0200

    Update widget_checkbox.v

--- src/widget_checkbox.v
+++ src/widget_checkbox.v
@@ -203,10 +203,10 @@ pub fn (mut cb CheckBox) draw_device(d DrawDevice) {
 			println('CheckBox($cb.id): ($cb.x, $cb.y, $cb.width, $cb.height)')
 		}
 	}
-	if cb.style.bg_color != no_color {
-		d.draw_rect_filled(cb.x - (cb.width - cb.adj_width) / 2, cb.y - (cb.height - cb.adj_height) / 2,
-			cb.width, cb.height, cb.ui.window.bg_color) // cb.style.bg_color)
-	}
+	// if cb.style.bg_color != no_color {
+	d.draw_rect_filled(cb.x - (cb.width - cb.adj_width) / 2, cb.y - (cb.height - cb.adj_height) / 2,
+		cb.width, cb.height, cb.ui.window.bg_color) // cb.style.bg_color)
+	// }
 	d.draw_rect_filled(cb.x, cb.y, ui.check_mark_size, ui.check_mark_size, cb.style.bg_color) // progress_bar_color)
 	draw_device_inner_border(false, d, cb.x, cb.y, ui.check_mark_size, ui.check_mark_size,
 		false)
