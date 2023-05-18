1b4fcf75fddcf976a9e1cd0bd2b50ad5148483c4
R cqls <rdrouilh@gmail.com>
2023-05-18 09:40:59 +0200

    Update demo_chunkview.v

--- examples/demo_chunkview.v
+++ examples/demo_chunkview.v
@@ -4,28 +4,57 @@ import gx
 fn main() {
 	mut cv := ui.chunkview(
 		id: 'cv'
-		chunks: [ui.textchunk(0, 20, 'toto titi', 'red'), ui.textchunk(0, 40, 'toto titi',
-			'blue'),
-			ui.textchunk(0, 60, '😻🥰 😬🧿 🫥😴  ✔️💾', 'emoji'),
+		chunks: [
 			ui.parachunk(
-				y: 100
+				y: 20
+				margin: 5
+				content: [['h2', 'TextChunk']!]
+			),
+			ui.textchunk(0, 60, 'toto titi', 'red'),
+			ui.textchunk(0, 80, 'toto titi', 'blue'),
+			ui.textchunk(0, 100, '😻🥰 😬🧿 🫥😴  ✔️💾', 'emoji'),
+			ui.parachunk(
+				y: 200
+				margin: 5
+				content: [['h2', 'ParaChunk']!]
+			),
+			ui.parachunk(
+				y: 240
+				indent: 20
 				content: [['red', 'toto titi tata toto titi tata ']!,
 					['blue', 'toto titi tata toto titi tata toto titi tata ']!,
 					['emoji', '😻🥰😬 🧿🫥😴  ✔️💾']!]
-			)]
+			),
+		]
 	)
 	mut window := ui.window(
 		width: 800
 		height: 600
 		title: 'V UI: ChunkView'
 		layout: ui.column(
+			heights: [ui.compact, ui.stretch]
 			children: [
 				ui.rectangle(
-					height: 64
-					width: 64
+					height: 30
 					color: gx.rgb(255, 100, 100)
 				),
-				cv,
+				ui.row(
+					children: [
+						ui.rectangle(
+							width: 30
+							color: gx.rgb(255, 100, 100)
+						),
+						cv,
+						ui.rectangle(
+							width: 30
+							color: gx.rgb(255, 100, 100)
+						),
+					]
+				),
+				ui.rectangle(
+					height: 30
+					color: gx.rgb(255, 100, 100)
+				),
 			]
 		)
 	)
@@ -34,7 +63,7 @@ fn main() {
 	dtw.add_style(
 		id: 'red'
 		size: 20
-		font_name: 'fixed_bold_italic'
+		font_name: 'fixed_bold'
 		color: gx.red
 	)
 	// Add global style
@@ -43,6 +72,12 @@ fn main() {
 		size: 30
 		font_name: 'system'
 		color: gx.blue
+	)
+	// Add global style
+	window.add_style(
+		id: 'h2'
+		size: 40
+		font_name: 'fixed_bold_italic'
 	)
 	// emoji
 	window.add_style(
